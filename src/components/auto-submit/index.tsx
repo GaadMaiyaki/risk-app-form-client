034d1d11a032b54802158612f5f6945a93cfc617
Gblack-Hub <petershola18@gmail.com>
2023-02-16 14:50:39 +0100

    updated

---
+++ src/components/auto-submit/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import { FormikProps } from "formik";
+
+import { debounce } from "../../utils";
+
+const k = (v: any) => {
+  return Promise.resolve(() => 500);
+};
+
+//const k = async (body: any) => {
+//  fetch("https://backstageapi.enterpriseinsight.app:8085/v1", {
+//    method: "POST",
+//    body: JSON.stringify(body),
+//  })
+//    .then((r) => console.log(r))
+//    .catch((e) => {
+//      console.log(e, "this isan error man");
+//    });
+//};
+
+const AutoSubmit = ({
+  formik: { dirty, values },
+}: {
+  formik: FormikProps<{ [key: string]: any }>;
+}) => {
+  const d = debounce<{ [key: string]: any }>(k, 3000);
+
+  const handleAutoSubmit = React.useCallback((arg: any) => d(arg), []);
+
+  React.useEffect(() => {
+    if (dirty) {
+      handleAutoSubmit(values);
+    }
+  }, [values]);
+
+  return null;
+};
+
+export default AutoSubmit;
