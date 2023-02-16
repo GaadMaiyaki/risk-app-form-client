0d976a936c94b0449206ea2ed71316d6469641dd
Gblack-Hub <petershola18@gmail.com>
2023-02-16 14:53:45 +0100

    updated

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.scss";
+import App from "./App";
+
+import { BrowserRouter as Router } from "react-router-dom";
+
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <React.Suspense fallback={<div>booting up...</div>}>
+      <Router>
+        <App />
+      </Router>
+    </React.Suspense>
   </React.StrictMode>
 );
 
@@ -17,3 +26,169 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+//import React from "react";
+
+//import Pagination from "../../components/resuable/pagination";
+
+//import { fields } from "../../data";
+
+//import { useRefs } from "./../../hooks/useRefs";
+//import { structredFormGroup } from "./../../utils/index";
+
+//const FormPage = () => {
+//  const groupedFields = structredFormGroup(fields);
+
+//  const len = Object.keys(groupedFields || []);
+
+//  const [currentSection, setCurrentSection] = React.useState(1);
+
+//  const currentGroup = groupedFields?.[currentSection] || [];
+
+//  const refs: any = useRefs(currentGroup);
+
+//  const eV: Array<any> = Object.values(currentGroup);
+
+//  const rF = fields.reduce((a: any, b: any) => {
+//    a[b.name] = b.value;
+
+//    return a;
+//  }, {});
+
+//  return (
+//    <div>
+//      <button
+//        onClick={() => {
+//          refs.current["value4"].current.scrollIntoView();
+//        }}
+//      >
+//        click
+//      </button>
+//      FormPage
+//      {/*<div className="my-5 py-5">
+//        <div className="bg-primary text-white p-4 my-5">
+//          {currentGroup?.[1]?.[0]?.section_name}
+//        </div>
+//        {Array.from({ length: eV.length }).map((v, i: number) => {
+//          return (
+//            <div className="my-5 bg-dark text-white shadow-sm">
+//              {eV[i].map((v: any) => {
+//                //want to enable the field only if the value of such field
+//                //is equal to its dependency value
+
+//                const showing =
+//                  !!v.value_dep &&
+//                  v.value_dep.some(({ name, value }: any) => {
+//                    return rF[name] === value;
+//                  });
+
+//                const disabled =
+//                  !!v.dependencies &&
+//                  v.dependencies.some((v: string) => {
+//                    return !rF[v] || rF[v] === "";
+//                  });
+
+//                if (!!v.value_dep) {
+//                  const showing = v.value_dep.some(({ name, value }: any) => {
+//                    console.log(
+//                      rF[name],
+//                      value,
+//                      "cheeck man",
+//                      rF[name] === value
+//                    );
+//                    return rF[name] === value;
+//                  });
+
+//                  if (showing) {
+//                    return (
+//                      <div className="my-5" ref={refs.current[v.value]}>
+//                        {v.value}
+//                        <input value={v.value || ""} disabled={disabled} />
+//                      </div>
+//                    );
+//                  }
+//                } else {
+//                  return (
+//                    <>
+//                      {
+//                        <div className="my-5" ref={refs.current[v.value]}>
+//                          {v.value}
+//                          <input value={v.value || ""} disabled={disabled} />
+//                        </div>
+//                      }
+//                    </>
+//                  );
+//                }
+//              })}
+//            </div>
+//          );
+//        })}
+//      </div>*/}
+//      <div>
+//        {/*<Pagination
+//          pageCount={len.length}
+//          onPageChange={({ selected: i }: { selected: number }) => {
+//            console.log(i, "this is the pagination man");
+//            setCurrentSection(++i);
+//          }}
+//        />*/}
+//        {/*{len.map((v, i) => {
+//          return (
+//            <button
+//              onClick={() => {
+//                setCurrentSection(+v);
+//              }}
+//            >
+//              {v}
+//            </button>
+//          );
+//        })}*/}
+//      </div>
+//    </div>
+//  );
+//};
+
+//export default FormPage;
+
+
+//{
+//  formSections.map((section) => (
+//    <button
+//      className="btn btn-primary mx-1"
+//      disabled={
+//        //false
+//        !!s?.[section]
+//          ? false
+//          : criteria(formik) && +section !== currentFormSection
+//      }
+//      type="submit"
+//      onClick={() => {
+//        console.log(formik.isValid, "is valid mans");
+//        flushSync(() => {
+//          formik.validateForm();
+//        });
+
+//        flushSync(() => {
+//          handleErr(formik.errors);
+//        });
+
+//        flushSync(() => {
+//          if (!!s?.[currentFormSection] || !!s[section]) {
+//            handleSectionChange(+section, "default");
+//          }
+//        });
+
+//        flushSync(() => {
+//          setS((p: any) => {
+//            return {
+//              ...p,
+//              [currentFormSection]: formik.isValid,
+//            };
+//          });
+//        });
+//      }}
+//    >
+//      {section}
+//    </button>
+//  ));
+//}
