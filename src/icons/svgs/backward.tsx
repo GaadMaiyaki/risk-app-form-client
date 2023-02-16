bc822ac93815af674556d563b6a31701ee520657
Gblack-Hub <petershola18@gmail.com>
2023-02-16 14:59:45 +0100

    updated

---
+++ src/icons/svgs/backward.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const BackwardSvg = ({ color }: { color: string }) => {
+  return (
+    <svg
+      width="7"
+      height="12"
+      viewBox="0 0 7 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M6 11L1 6L6 1"
+        stroke={color}
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default BackwardSvg;
