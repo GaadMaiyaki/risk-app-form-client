c1e983aebba24fe201e25491dc38a8a0f070adf9
Gblack-Hub <petershola18@gmail.com>
2023-02-17 09:54:36 +0100

    updated

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -6,36 +6,38 @@ import { parseClassName } from "./../../utils";
 
 const Header = () => {
   return (
-    <header>
-      <nav
-        className={`d-flex justify-content-between align-items-center flex-column flex-lg-row flex-xl-row flex-xxl-row ${styles.wrapper} py-3 px-2 px-md-5 px-lg-5 px-xl-5 px-xxl-5`}
+    <section
+      className={parseClassName([
+        "d-flex justify-content-between align-items-center flex-column flex-lg-row flex-xl-row flex-xxl-row",
+        "py-3 px-2 px-md-5 px-lg-5 px-xl-5 px-xxl-5",
+        styles.wrapper,
+      ])}
+    >
+      <div>
+        <img
+          src="/logo.png"
+          loading="lazy"
+          title="pillarrisk partners logo"
+          alt="A logo of backstage pillarrisk"
+          className="img-fluid"
+        />
+      </div>
+
+      <div
+        className={parseClassName([
+          "text-center mt-4 md-lg-0 mt-xl-0 mt-xxl-0",
+          styles.text,
+        ])}
       >
-        <div>
-          <img
-            src="/logo.png"
-            loading="lazy"
-            title="pillarrisk partners logo"
-            alt="Backstage pillarrisk logo"
-            className="img-fluid"
-          />
-        </div>
+        <h3>Loan Origination Insurance Review Order Form</h3>
 
-        <div
-          className={parseClassName([
-            "text-center mt-4 md-lg-0 mt-xl-0 mt-xxl-0",
-            styles.text,
-          ])}
-        >
-          <h3>Loan Origination Insurance Review Order Form</h3>
-
-          <div className="mt-2 px-0 px-lg-5 px-xl-5 px-xxl-5">
-            Please complete this form in its entirety and upload all necessary
-            attachments. All information left blank, and attachments not
-            uploaded at submission must be provided to Pillar Risk upon receipt
-          </div>
-        </div>
-      </nav>
-    </header>
+        <p className="mt-2 px-0 px-lg-5 px-xl-5 px-xxl-5">
+          Please complete this form in its entirety and upload all necessary
+          attachments. All information left blank, and attachments not uploaded
+          at submission must be provided to Pillar Risk upon receipt
+        </p>
+      </div>
+    </section>
   );
 };
 
