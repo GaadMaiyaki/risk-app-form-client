853693e5b7153be44f139adb96ed80bb28222692
Gblack-Hub <petershola18@gmail.com>
2023-02-21 09:15:28 +0100

    updated.

--- src/components/form-components/index.tsx
+++ src/components/form-components/index.tsx
@@ -1,26 +1,41 @@
 import React from "react";
 
 import { ErrorMessage, FormikProps } from "formik";
+import InputField from "../resuable/input-field";
 
 interface IFormComponents {
   field: { [key: string]: any };
   formik: FormikProps<any>;
 }
 
-const E = ({ formik, field }: { formik: any; field: any }) => {
-  return <div>{formik.errors[field.name]}</div>;
+//const E = ({ formik, field }: { formik: any; field: any }) => {
+//  return <div>{formik.errors[field.name]}</div>;
+//};
+
+type TFieldType = "text" | "file" | "date" | "popform";
+
+const fieldComponentPicker = (
+  type: TFieldType,
+  field: { [key: string]: any },
+  ref: React.MutableRefObject<any>,
+  formik: FormikProps<any>
+): JSX.Element | undefined => {
+  switch (type) {
+    case "text": {
+      return <InputField formik={formik} field={field} ref={ref} />;
+    }
+    default:
+      throw new Error("invalid field type");
+  }
 };
 
 const FormComponents = React.forwardRef(
   ({ field, formik }: IFormComponents, ref: any) => {
-    console.log(field.section, field.name, "this is handle change");
-
     return (
       <div className="col-12 my-5">
-       <div>form field</div>
-        {/*<E formik={formik} field={field} />*/}
+        {fieldComponentPicker(field.type, field, ref, formik)}
 
-        {/*{<ErrorMessage name={field.name} />}*/}
+        {<ErrorMessage name={field.name} />}
       </div>
     );
   }
