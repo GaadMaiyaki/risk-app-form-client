bd57f63e8c0d2356757adbbfffc007434f355880
Gblack-Hub <petershola18@gmail.com>
2023-02-27 09:44:41 +0100

    updated.

---
+++ src/components/resuable/field-wrapper/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import { parseClassName } from "./../../../utils";
+
+import styles from "./index.module.scss";
+
+interface IFieldWrapper {
+  children: React.ReactNode;
+  withYMargin?: boolean;
+  withJustify?: boolean;
+}
+
+export const FieldWrapper = ({
+  children,
+  withYMargin,
+  withJustify,
+}: IFieldWrapper) => {
+  return (
+    <div
+      className={parseClassName([
+        "col-12 col-md-9 col-lg-6 col-xl-6 col-xxl-6 pl-2",
+        styles.wrapper,
+      ])}
+    >
+      <aside
+        className={parseClassName([
+          "d-flex align-items-center",
+          withJustify ? "flex-wrap" : "justify-content-between",
+          withYMargin ? "my-1" : "",
+        ])}
+      >
+        {children}
+      </aside>
+    </div>
+  );
+};
+
+export default FieldWrapper;
