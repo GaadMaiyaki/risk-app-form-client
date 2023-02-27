6d1d869650e58e34da6396c652fb3ce30b239f39
Gblack-Hub <petershola18@gmail.com>
2023-02-27 09:46:04 +0100

    updated.

---
+++ src/icons/svgs/info.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const InfoSvg = () => {
+  return (
+    <svg
+      width="14"
+      height="14"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_46_3013)">
+        <path
+          d="M6 5.75V8.25M6 3.755L6.005 3.7495M6 11C8.7615 11 11 8.7615 11 6C11 3.2385 8.7615 1 6 1C3.2385 1 1 3.2385 1 6C1 8.7615 3.2385 11 6 11Z"
+          stroke="#181818"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_46_3013">
+          <rect width="12" height="12" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export default InfoSvg;
