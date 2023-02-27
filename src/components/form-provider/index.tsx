cb0bf098f4b2e76851279e9b0f0fc6bbef38365d
Gblack-Hub <petershola18@gmail.com>
2023-02-27 09:44:23 +0100

    updated.

--- src/components/form-provider/index.tsx
+++ src/components/form-provider/index.tsx
@@ -1,21 +1,18 @@
 import React from "react";
 
-import { flushSync } from "react-dom";
-
 import { Formik, Form, FormikProps } from "formik";
 
 import * as Yup from "yup";
 
 import { useFormProcessor } from "./../../hooks/useFormProcessor";
+import { useRefs } from "./../../hooks/useRefs";
+
+import Tabs from "../resuable/tabs";
+import Button from "../resuable/button";
 
 import FormProccessor from "../form-processor";
 
-import { extractDataValue, validatorProcessor } from "../../utils";
-
-import { useRefs } from "./../../hooks/useRefs";
-import Button from "../resuable/button";
-import { flatData } from "./../../utils/index";
-import Tabs from "../resuable/tabs";
+import { extractDataValue, handleError, validatorProcessor } from "../../utils";
 
 interface IFormProvider {
   data: Array<any>;
@@ -40,24 +37,13 @@ const FormProvider = ({ data }: IFormProvider) => {
     [key: string]: React.MutableRefObject<any>;
   } = useRefs(data);
 
-  const handleErr = (errors: any) => {
-    const errorsArr = Object.keys(errors || {});
-
-    errorsArr?.length > 0 &&
-      errorsArr.forEach((e) => {
-        refs[e].current?.scrollIntoView({
-          block: "center",
-          inline: "nearest",
-        });
-        refs[e].current?.focus();
-      });
-  };
+  console.log(currentSection, "this is current section man");
 
   return (
     <Tabs data={groupedFields} section={section}>
       <Formik
         initialValues={extractDataValue(data)}
-        validationSchema={validatorProcessor(Yup, currentSection)}
+        validationSchema={validatorProcessor(currentSection)}
         validateOnMount
         onSubmit={(v, f: any) => {
           console.log(v, "this is value", f, "this is error");
@@ -65,67 +51,69 @@ const FormProvider = ({ data }: IFormProvider) => {
       >
         {(formik: FormikProps<any>) => {
           return (
-            <>
-              {console.log(formik.isValid, formik, formik.isSubmitting)}
-              <Form>
-                <FormProccessor
-                  fields={currentSection}
-                  formik={formik}
-                  refs={refs}
+            <Form>
+              <FormProccessor
+                fields={currentSection}
+                formik={formik}
+                refs={refs}
+              />
+              {console.log(formik, "formik") as any}
+
+              <aside className="my-5 text-center">
+                <Button
+                  onClick={() => {
+                    formik.isValid && handleSectionChange("previous");
+
+                    !formik.isValid && handleError(formik.errors, refs);
+
+                    formik.setSubmitting(false);
+                  }}
+                  type="submit"
+                  title=""
+                  color="primary"
+                  size="lg"
+                  label="Previous"
+                  disabled={!shouldPrevious}
+                  classes="bg-none"
+                  renderComponent={() => (
+                    <span>{shouldPrevious && getPreviousSectionName()}</span>
+                  )}
                 />
 
-                <aside className="my-5 text-center">
+                <Button
+                  onClick={() => {
+                    !formik.isValid && handleError(formik.errors, refs);
+
+                    formik.isValid && handleSectionChange("next");
+
+                    formik.setSubmitting(false);
+                  }}
+                  type="submit"
+                  title=""
+                  color="primary"
+                  size="lg"
+                  label="Next"
+                  disabled={!shouldNext}
+                  classes="mx-2 bg-none"
+                  renderComponent={() => (
+                    <span>{shouldNext && getNextSectionName()}</span>
+                  )}
+                />
+              </aside>
+
+              <aside className="text-center">
+                {section === formSections.length && (
                   <Button
-                    onClick={() => {
-                      formik.isValid && handleSectionChange("previous");
-
-                      !formik.isValid && handleErr(formik.errors);
-                    }}
                     type="submit"
-                    title=""
-                    color="primary"
-                    size="lg"
-                    label="Previous"
-                    disabled={!shouldPrevious}
-                    classes="bg-none"
-                    renderComponent={() => (
-                      <span>{shouldPrevious && getPreviousSectionName()}</span>
-                    )}
-                  />
-
-                  <Button
-                    onClick={() => {
-                      !formik.isValid && handleErr(formik.errors);
-
-                      formik.isValid && handleSectionChange("next");
-                    }}
-                    type="submit"
-                    title=""
+                    title="Submit Form"
                     color="primary"
                     size="lg"
                     label="Next"
-                    disabled={!shouldNext}
-                    classes="mx-2 bg-none"
-                    renderComponent={() => (
-                      <span>{shouldNext && getNextSectionName()}</span>
-                    )}
+                    classes="w-100 mb-3"
                   />
-                </aside>
-
-                <aside className="text-center">
-                  {section === formSections.length && (
-                    <Button
-                      type="submit"
-                      title="Submit Form"
-                      color="primary"
-                      size="lg"
-                      label="Next"
-                      classes="w-100 mb-3"
-                    />
-                  )}
-                </aside>
-              </Form>
-            </>
+                )}
+              </aside>
+            </Form>
           );
         }}
       </Formik>
