f837758b29870f1a5739c39b1d34f14cbc25e074
Gblack-Hub <petershola18@gmail.com>
2023-02-28 14:12:51 +0100

    updated.

---
+++ src/services/axios/index.ts
@@ -0,0 +1,52 @@
+import axios, { AxiosResponse, InternalAxiosRequestConfig } from "axios";
+
+import LocalStorageService from "../local-storage";
+
+const { REACT_APP_BASEURL, REACT_APP_USER }: any = process.env;
+
+const axiosInstance = axios.create({
+  baseURL: REACT_APP_BASEURL,
+});
+
+axiosInstance.interceptors.request.use(
+  (
+    config: InternalAxiosRequestConfig<any>
+  ): InternalAxiosRequestConfig<any> => {
+    const token = LocalStorageService.get(REACT_APP_USER);
+
+    if (!!token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (res: AxiosResponse<any, any>) => {
+    return Promise.resolve(res);
+  },
+  async (error) => {
+    const errConfig = error?.config;
+
+    if (!error.response) {
+      return Promise.reject(error);
+    }
+
+    if (errConfig?.url !== "/login" && error.response) {
+      if (error.response.status === 401 || error.response.status === 403) {
+        LocalStorageService.remove(REACT_APP_USER);
+        window.location.href = "/";
+
+        return Promise.reject(error);
+      }
+
+      return Promise.reject(error);
+    }
+  }
+);
+
+export default axiosInstance;
