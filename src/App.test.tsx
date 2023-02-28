981a43907fec639ccb8b7b9f60491b40f27914cd
Gblack-Hub <petershola18@gmail.com>
2023-02-28 10:18:05 +0100

    updated.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import {render} from "@testing-library/react";
+
+import { render } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
 
 import App from "./App";
 
 import { BrowserRouter as Router } from "react-router-dom";
-import { act } from "react-dom/test-utils";
 
 test("should render without crashing", async () => {
   act(() => {
