9559deb7d4662c2b20a479ba066ed1bae9b58d2d
Gblack-Hub <petershola18@gmail.com>
2023-02-28 14:12:39 +0100

    updated.

--- src/icons/svgs/info.tsx
+++ src/icons/svgs/info.tsx
@@ -9,13 +9,13 @@ const InfoSvg = () => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g clip-path="url(#clip0_46_3013)">
+      <g clipPath="url(#clip0_46_3013)">
         <path
           d="M6 5.75V8.25M6 3.755L6.005 3.7495M6 11C8.7615 11 11 8.7615 11 6C11 3.2385 8.7615 1 6 1C3.2385 1 1 3.2385 1 6C1 8.7615 3.2385 11 6 11Z"
           stroke="#181818"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
