04d6842499d0c9dd8aaf0432f4ba88e682e67e2c
Gblack-Hub <petershola18@gmail.com>
2023-03-01 09:54:18 +0100

    updated.

--- src/services/http-method/index.ts
+++ src/services/http-method/index.ts
@@ -1,16 +1,16 @@
-import axios from "axios";
+import axiosInstance from "../axios";
 
 class HTTP {
   static async post(path: string, data: any) {
     try {
-      return await axios.post(path, data);
+      return await axiosInstance.post(path, data);
     } catch (err: any) {
       throw new Error(err.response ? err.response : err);
     }
   }
   static async get(path: string) {
     try {
-      return await axios.get(path);
+      return await axiosInstance.get(path);
     } catch (err: any) {
       throw new Error(err.response ? err.response : err);
     }
