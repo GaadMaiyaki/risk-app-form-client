ca2efe715a3b562447a9da83a8c2988fc9647e8e
Gblack-Hub <petershola18@gmail.com>
2023-03-20 10:09:14 +0100

    updated.

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 
+import styles from "./index.module.scss";
+
+import { parseClassName } from "./../../utils/";
+
+const year = new Date().getFullYear();
+
 const Footer = () => {
-  return <div>this is footer</div>;
+  return (
+    <section className={parseClassName([styles.wrapper, "text-center"])}>
+      <div className="py-4">
+        <aside>Pillar Risk Partners LLC</aside>
+        <aside> @{year} by Pillar Risk</aside>
+      </div>
+
+      <aside className="pb-2">Privacy Policy</aside>
+    </section>
+  );
 };
 
 export default Footer;
-
