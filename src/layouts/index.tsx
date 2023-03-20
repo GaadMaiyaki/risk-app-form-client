e1138b7c4b374f115f135fbbcd8b92524d7d98bd
Gblack-Hub <petershola18@gmail.com>
2023-03-20 10:13:44 +0100

    updated.

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,23 +1,29 @@
-import React from "react";
-
 import { Outlet } from "react-router";
 
-//import Footer from "../components/footer";
+import Footer from "../components/footer";
+import FormFooter from "../components/form-footer";
+import FormHeader from "../components/form-header";
 import Header from "../components/header";
 
 const HeaderFooterLayout = () => {
   return (
-    <section className="container">
+    <>
       <header>
         <Header />
       </header>
 
-      <main>
-        <Outlet />
+      <main className="bg-c py-4 mx-0 mx-lg-5 mx-xl-5 mx-xxl-5">
+        <section className="container">
+          <FormHeader />
+          <Outlet />
+        </section>
       </main>
 
-      {/*<Footer />*/}
-    </section>
+      <footer>
+        <FormFooter />
+        <Footer />
+      </footer>
+    </>
   );
 };
 
