198e5f61b4efaa1525e5460386e322c7cd7c1db0
Gblack-Hub <petershola18@gmail.com>
2023-03-20 10:15:18 +0100

    updated.

--- src/pages/form/index.tsx
+++ src/pages/form/index.tsx
@@ -4,6 +4,8 @@ import FormProvider from "../../components/form-provider";
 
 import { fields } from "../../data";
 
+import { useGet } from "../../services/queries";
+
 const makeApicall = () => {
   return new Promise((resolve, reject) => {
     setTimeout(() => resolve(fields), 500);
@@ -11,30 +13,35 @@ const makeApicall = () => {
 };
 
 const FormPage = () => {
-  const [data, setData] = React.useState([]);
+  //const [data, setData] = React.useState([]);
 
-  const [isLoading, setIsLoading] = React.useState(true);
+  //const [isLoading, setIsLoading] = React.useState(true);
 
-  const [isError, setIsError] = React.useState(false);
+  //const [isError,setIsError]=React.useState(false);
 
-  React.useEffect(() => {
-    makeApicall()
-      .then((data: any) => {
-        setData(data);
-        setIsLoading(false);
-      })
-      .catch((e) => setIsError(true));
-  }, []);
+  const { isLoading, isError, data, error, refetch }: any =
+    useGet("/client/order/form");
+
+  //React.useEffect(() => {
+  //  makeApicall()
+  //    .then((data: any) => {
+  //      setData(data);
+  //      setIsLoading(false);
+  //    })
+  //    .catch((e) => setIsError(true));
+  //}, []);
 
   if (isLoading) {
-    return <div>Please wait...</div>;
+    return <div className="text-center py-5">Please wait...</div>;
   }
 
   if (isError) {
     return (
-      <div>
-        An error occured...
-        <button onClick={() => console.log("retrying")}>retry</button>
+      <div className="text-center text-danger py-5">
+        <div>{error.message}</div>
+        <button className="btn btn-success mt-2" onClick={() => refetch()}>
+          retry
+        </button>
       </div>
     );
   }
