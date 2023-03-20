4137692d79231716d1739fe5508b3f5faae03a39
Gblack-Hub <petershola18@gmail.com>
2023-03-20 10:15:05 +0100

    updated.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ const HomePage = () => {
   return (
     <section
       className={parseClassName([
-        "row justify-content-center mt-5",
+        "row justify-content-center py-4",
         styles.wrapper,
       ])}
     >
