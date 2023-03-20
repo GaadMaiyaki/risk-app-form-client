4ac765fd870f0b6d4aa402ca4cff5760f827fc91
Gblack-Hub <petershola18@gmail.com>
2023-03-20 10:09:28 +0100

    updated.

---
+++ src/components/form-footer/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import styles from "./index.module.scss";
+
+import { parseClassName } from "./../../utils";
+
+const FormFooter = () => {
+  return (
+    <section className={parseClassName([styles.wrapper, "text-center py-2"])}>
+      Forms submissions are secured using TLS 1.2 SSL and are stored in a
+      database that is encrypted at rest
+    </section>
+  );
+};
+
+export default FormFooter;
