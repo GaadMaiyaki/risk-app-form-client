5f46dead76452c9f5d4ddcb88e6c2b483e494396
Gblack-Hub <petershola18@gmail.com>
2023-03-21 09:36:27 +0100

    updated.

--- src/components/form-footer/index.tsx
+++ src/components/form-footer/index.tsx
@@ -6,7 +6,9 @@ import { parseClassName } from "./../../utils";
 
 const FormFooter = () => {
   return (
-    <section className={parseClassName([styles.wrapper, "text-center py-2"])}>
+    <section
+      className={parseClassName([styles.wrapper, "text-center py-2 my-3"])}
+    >
       Forms submissions are secured using TLS 1.2 SSL and are stored in a
       database that is encrypted at rest
     </section>
