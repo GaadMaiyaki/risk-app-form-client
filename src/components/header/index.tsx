562a24c5d0c78f48a8d23498130a88682a7b7307
Gblack-Hub <petershola18@gmail.com>
2023-03-24 10:57:24 +0100

    updated.

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -13,20 +13,25 @@ const Header = () => {
       <section
         className={parseClassName([
           "d-flex justify-content-between align-items-center",
-          "flex-column flex-md-row flex-lg-row flex-xl-row flex-xxl-row",
+          "flex-row flex-md-row flex-lg-row flex-xl-row flex-xxl-row",
           "py-2 px-2 px-md-5 px-lg-5 px-xl-5 px-xxl-5 mb-1",
           styles.innerWrapper,
         ])}
       >
-        <div>
+        <div className="">
           <button
             aria-label="go back to home page"
             title="go back to home page"
-            className={styles.btn}
+            className={parseClassName([
+              styles.btn,
+              " d-flex align-items-center",
+            ])}
           >
             <GoBackSvg />
 
-            <span className="ms-2">Back to Home Page</span>
+            <span className="ms-2 d-none d-md-block d-lg-block d-xl-block d-xxl-block">
+              Back to Home Page
+            </span>
           </button>
         </div>
 
