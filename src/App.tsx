0351e3a0416fc695127127efa6ffb8af2f65f3dc
Gblack-Hub <petershola18@gmail.com>
2023-03-24 10:54:17 +0100

    updated.

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,18 @@ import "./App.scss";
 
 import useCustomRoutes from "./routes/index";
 
+//import MyModal from "@gaadmaiyaki/simple-react-package";
+
 function App() {
   const routes = useCustomRoutes();
 
-  return <section className="app">{routes}</section>;
+  return (
+    <section className="app">
+      {routes}
+
+      {/*<MyModal>hello world</MyModal>*/}
+    </section>
+  );
 }
 
 export default App;
