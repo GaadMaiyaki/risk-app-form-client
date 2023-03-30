eb5b815d785181545e820350eb07798a1d773f55
Gblack-Hub <petershola18@gmail.com>
2023-03-30 10:06:23 +0100

    updated.

---
+++ src/components/hoc/index.ts
@@ -0,0 +1,5 @@
+import withProtection from "./withProtection";
+import withProtectionOnce from "./withProtectionOnce";
+import withClientName from "./withClientName";
+
+export { withProtection, withProtectionOnce, withClientName };
