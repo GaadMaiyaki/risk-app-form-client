b5c68ffbb2a8cf436baa59e8ad6d3da12bfba724
Gblack-Hub <petershola18@gmail.com>
2023-03-30 10:48:22 +0100

    updated.

--- src/pages/form/index.tsx
+++ src/pages/form/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import FormProvider from "../../components/form-provider";
 
-import { withProtection } from "../../components/hoc";
+import { withProtection } from "../../hoc";
 
 import { fields } from "../../data";
 
