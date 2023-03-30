6f67f88ecb06e65a168e35f542b864d9da577224
Gblack-Hub <petershola18@gmail.com>
2023-03-30 16:06:22 +0100

    updated.

---
+++ src/hooks/useWindowWidth.ts
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const useWindowWidth = () => {
+  const [width, setWidth] = React.useState(window.innerWidth);
+
+  const handleWidth = () => setWidth(window.innerWidth);
+
+  React.useEffect(() => {
+    window.addEventListener("resize", handleWidth, { once: true });
+  });
+
+  return width;
+};
