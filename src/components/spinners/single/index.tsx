91cd89abbf602320fac4f85971dbcc5945795c19
Gblack-Hub <petershola18@gmail.com>
2023-03-30 16:04:07 +0100

    updated.

---
+++ src/components/spinners/single/index.tsx
@@ -0,0 +1,21 @@
+import { parseClassName } from "../../../utils";
+
+import styles from "./index.module.scss";
+
+export const SingleSpinner = () => {
+  return (
+    <div
+      className={parseClassName([
+        styles.loading,
+        "d-flex flex-column align-items-center justify-content-center",
+      ])}
+    >
+      <div className="d-flex align-items-center mb-2">
+        <div className="spinner-grow spinner-grow-md mb-2 text-success"></div>
+      </div>
+      <div>Please wait...</div>
+    </div>
+  );
+};
+
+export default SingleSpinner;
