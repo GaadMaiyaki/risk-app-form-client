dd1a21a323aa0ecbee1860b4ed1d8c2eb05617fd
Gblack-Hub <petershola18@gmail.com>
2023-03-30 10:06:35 +0100

    updated.

---
+++ src/components/hoc/withProtection.tsx
@@ -0,0 +1,24 @@
+import { Navigate } from "react-router-dom";
+
+import LocalStorageService from "../../services/local-storage";
+
+const withProtection = <T extends {}>(Component: React.FC<T>) => {
+  return (props: T) => {
+    const isAuth = LocalStorageService.get(
+      process.env.REACT_APP_USER as string
+    );
+
+    if (!isAuth) return <Navigate to="/" replace />;
+
+    const ModifiedComponent = Component;
+
+    const hocComponentName =
+      Component.displayName || Component.name || "Component";
+
+    ModifiedComponent.displayName = hocComponentName;
+
+    return <ModifiedComponent {...props} />;
+  };
+};
+
+export default withProtection;
