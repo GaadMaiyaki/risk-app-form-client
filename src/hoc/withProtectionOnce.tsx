cc75d67ff103f6b9246d8eee884ec194a16506dc
Gblack-Hub <petershola18@gmail.com>
2023-03-30 10:48:09 +0100

    updated.

---
+++ src/hoc/withProtectionOnce.tsx
@@ -0,0 +1,24 @@
+import { Navigate, NavigateProps } from "react-router-dom";
+
+import LocalStorageService from "../services/local-storage";
+
+const withProtectionOnce = <T extends {}>(Component: React.FC<T>) => {
+  return (props: T) => {
+    const isAuth = LocalStorageService.get(
+      process.env.REACT_APP_USER as string
+    );
+
+    if (isAuth) return <Navigate to="/form" replace />;
+
+    const ModifiedComponent: React.FC<T> = Component;
+
+    const hocComponentName =
+      Component.displayName || Component.name || "Component";
+
+    ModifiedComponent.displayName = hocComponentName;
+
+    return <ModifiedComponent {...props} />;
+  };
+};
+
+export default withProtectionOnce;
