4300d503c663e707af5f67f8d38deaf603f0a60a
Gblack-Hub <petershola18@gmail.com>
2023-03-30 10:06:13 +0100

    updated.

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,10 +2,17 @@ import React from "react";
 
 import styles from "./index.module.scss";
 
-import { getClientName, parseClassName } from "./../../utils";
+import { parseClassName } from "./../../utils";
+
+import { withClientName } from "./../hoc";
+
 import GoBackSvg from "../../icons/svgs/back";
 
-const Header = () => {
+interface IHeaderProps {
+  clientName?: "peter" | "Welcome";
+}
+
+const Header = ({ clientName }: IHeaderProps) => {
   return (
     <section
       className={parseClassName([styles.wrapper, "position-fixed w-100"])}
@@ -46,56 +53,11 @@ const Header = () => {
         </div>
 
         <div className={parseClassName(["text-end", styles.text])}>
-          {getClientName() ?? "Welcome"}
+          {clientName}
         </div>
       </section>
     </section>
   );
 };
 
-export default Header;
-
-//import React from "react";
-
-//import styles from "./index.module.scss";
-
-//import { parseClassName } from "./../../utils";
-
-//const Header = () => {
-//  return (
-//    <section
-//      className={parseClassName([
-//        "d-flex justify-content-between align-items-center flex-column flex-lg-row flex-xl-row flex-xxl-row",
-//        "py-3 px-2 px-md-5 px-lg-5 px-xl-5 px-xxl-5",
-//        styles.wrapper,
-//      ])}
-//    >
-//      <div>
-//        <img
-//          src="/logo.png"
-//          loading="lazy"
-//          title="pillarrisk partners logo"
-//          alt="A logo of backstage pillarrisk"
-//          className="img-fluid"
-//        />
-//      </div>
-
-//      <div
-//        className={parseClassName([
-//          "text-center mt-4 md-lg-0 mt-xl-0 mt-xxl-0",
-//          styles.text,
-//        ])}
-//      >
-//        <h3>Loan Origination Insurance Review Order Form</h3>
-
-//        <p className="mt-2 px-0 px-lg-5 px-xl-5 px-xxl-5">
-//          Please complete this form in its entirety and upload all necessary
-//          attachments. All information left blank, and attachments not uploaded
-//          at submission must be provided to Pillar Risk upon receipt
-//        </p>
-//      </div>
-//    </section>
-//  );
-//};
-
-//export default Header;
+export default withClientName(Header);
