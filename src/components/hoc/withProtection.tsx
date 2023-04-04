44ea4c5cc860998da937dc6333f6219df63ec4b8
GaadMaiyaki <65074080+GaadMaiyaki@users.noreply.github.com>
2023-04-04 10:27:09 +0100

    Update withProtection.tsx

--- src/components/hoc/withProtection.tsx
+++ src/components/hoc/withProtection.tsx
@@ -1,14 +1,14 @@
-import { Navigate } from "react-router-dom";
+// import { Navigate } from "react-router-dom";
 
-import LocalStorageService from "../../services/local-storage";
+// import LocalStorageService from "../../services/local-storage";
 
 const withProtection = <T extends {}>(Component: React.FC<T>) => {
   return (props: T) => {
-    const isAuth = LocalStorageService.get(
-      process.env.REACT_APP_USER as string
-    );
+//     const isAuth = LocalStorageService.get(
+//       process.env.REACT_APP_USER as string
+//     );
 
-    if (!isAuth) return <Navigate to="/" replace />;
+//     if (!isAuth) return <Navigate to="/" replace />;
 
     const ModifiedComponent = Component;
 
